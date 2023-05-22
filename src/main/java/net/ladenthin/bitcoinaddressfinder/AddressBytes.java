58ac18272d073cb55bd8c88ef97aa390a2c6c556
cjherm <akbigchris@gmail.com>
2023-05-22 18:49:40 +0200

    Introduced constructor in "AddressBytes".

--- src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
@@ -1,4 +1,14 @@
 package net.ladenthin.bitcoinaddressfinder;
 
+import java.math.BigInteger;
+
 public class AddressBytes {
+
+    private final BigInteger privateKey;
+    private final byte[] uncompressed;
+
+    public AddressBytes(BigInteger privateKey, byte[] uncompressed){
+        this.privateKey = privateKey;
+        this.uncompressed = uncompressed;
+    }
 }
