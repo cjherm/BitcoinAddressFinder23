b42afc541850c79fce811e48fa5deb1ba37f5e4f
cjherm <akbigchris@gmail.com>
2023-07-17 00:26:22 +0200

    Added missing constant in "AddressBytes".

--- src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
@@ -4,6 +4,12 @@ import java.math.BigInteger;
 
 public class AddressBytes {
 
+    public static final AddressBytes INVALID_ADDRESS = null;
+    public static final int SINGLE_SHA256_BYTE_LEN = 32;
+    public static final int DOUBLE_SHA256_BYTE_LEN = 2 * SINGLE_SHA256_BYTE_LEN;
+    public static final int PUB_KEY_WITHOUT_PARITY_WITH_DOUBLE_SHA256_BYTE_LEN = PublicKeyBytes.TWO_COORDINATES_NUM_BYTES + DOUBLE_SHA256_BYTE_LEN;
+    // TODO store public key with parity in opencl kernel result
+    public static final int PUB_KEY_WITH_PARITY_WITH_SHA256_BYTE_LEN = PUB_KEY_WITHOUT_PARITY_WITH_DOUBLE_SHA256_BYTE_LEN + 1;
     private final BigInteger privateKey;
     private final byte[] uncompressed;
 
