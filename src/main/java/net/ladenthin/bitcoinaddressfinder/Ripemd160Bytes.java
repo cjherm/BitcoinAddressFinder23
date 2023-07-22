3a1ea6b41d41966d75814f3ffe2d59e8c2d0f112
cjherm <akbigchris@gmail.com>
2023-07-22 14:50:49 +0200

    Introducing new data structure "Ripemd160Bytes" to store calculated RIPEMD-160 hashes coming from the OpenCL kernels.

---
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/Ripemd160Bytes.java
@@ -0,0 +1,11 @@
+package net.ladenthin.bitcoinaddressfinder;
+
+public class Ripemd160Bytes {
+
+    public static final int LENGTH_IN_BYTES = 20;
+
+    public byte[] getBytes() {
+        // TODO implement method
+        return null;
+    }
+}
