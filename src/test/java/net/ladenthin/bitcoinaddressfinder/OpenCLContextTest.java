f68ea5ba4386d80688d851df6e0f32be3687294a
cjherm <akbigchris@gmail.com>
2023-07-30 19:05:11 +0200

    Added new test "test_generateUntilSecondSha256Hash_specificSinglePrivateKey_bytewiseMode" to "OpenCLContextTest".

--- src/test/java/net/ladenthin/bitcoinaddressfinder/OpenCLContextTest.java
+++ src/test/java/net/ladenthin/bitcoinaddressfinder/OpenCLContextTest.java
@@ -685,4 +685,33 @@ public class OpenCLContextTest {
         assertThatResultBytesArray(result).isEqualTo(expected);
         assertThat(openCLContext.getErrorCodeString(), is(equalTo(ERROR_CODE_SUCCESS)));
     }
+
+    @Test
+    public void test_generateUntilSecondSha256Hash_specificSinglePrivateKey_bytewiseMode() {
+        // arrange
+        BigInteger[] specificSinglePrivateKey = TestHelper.transformHexStringToBigIntegerArray(PRIVATE_KEY_HEX_STRING);
+        OpenCLContext openCLContext = TestHelper.createOpenCLContext(CHUNK_MODE, OpenCLContext.GEN_BYTEWISE_SECOND_SHA256_MODE);
+        byte[] privateKey = TestHelper.transformHexStringToBytes(PRIVATE_KEY_HEX_STRING);
+        byte[] expectedPublicKey = TestHelper.calculatePublicKeyAsBytesFromPrivateKey(specificSinglePrivateKey[0]);
+        byte[] expectedFirstSha256 = TestHelper.calculateSha256FromByteArray(expectedPublicKey);
+        byte[] expectedRipemd160 = TestHelper.calculateRipemd160FromByteArray(expectedFirstSha256);
+        byte[] expectedDigestWithVersionByte = TestHelper.calculateDigestWithVersionByteFromByteArray(expectedRipemd160);
+        byte[] expectedSecondSha256 = TestHelper.calculateSha256FromByteArray(expectedDigestWithVersionByte);
+
+        // act
+        OpenCLGridResult openCLGridResult = openCLContext.createResult(specificSinglePrivateKey);
+        ResultBytes result = openCLGridResult.getResultBytes()[0];
+
+        // cleanup
+        openCLContext.release();
+        openCLGridResult.freeResult();
+
+        // assert
+        assertThat(result.getPrivateKeyBytes(), is(equalTo(privateKey)));
+        assertThat(result.getPublicKeyBytes(), is(equalTo(expectedPublicKey)));
+        assertThat(result.getFirstSha256BytesBytes(), is(equalTo(expectedFirstSha256)));
+        assertThat(result.getRipemd160BytesBytes(), is(equalTo(expectedRipemd160)));
+        assertThat(result.getSecondSha256Bytes(), is(equalTo(expectedSecondSha256)));
+        assertThat(openCLContext.getErrorCodeString(), is(equalTo(ERROR_CODE_SUCCESS)));
+    }
 }
