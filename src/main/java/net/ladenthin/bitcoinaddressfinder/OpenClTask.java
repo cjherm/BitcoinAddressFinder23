8bcb78c24cf89b1820d0a5b9e7a4ac952365993d
cjherm <akbigchris@gmail.com>
2023-08-02 21:33:08 +0200

    Linked new OpenCL kernel to correct calculation of result buffer size in "OpenClTask".

--- src/main/java/net/ladenthin/bitcoinaddressfinder/OpenClTask.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/OpenClTask.java
@@ -98,6 +98,8 @@ public class OpenClTask {
             return ResultBytes.NUM_BYTES_TOTAL_UNTIL_2ND_SHA256 * cProducer.getWorkSize();
         } else if (cProducer.kernelMode == OpenCLContext.GEN_BYTEWISE_3RD_SHA256_MODE) {
             return ResultBytes.NUM_BYTES_TOTAL_UNTIL_3RD_SHA256 * cProducer.getWorkSize();
+        } else if (cProducer.kernelMode == OpenCLContext.GEN_BYTEWISE_ADDRESS_MODE) {
+            return ResultBytes.NUM_BYTES_TOTAL_UNTIL_ADDRESS * cProducer.getWorkSize();
         } else {
             // TODO handle that case
             return -1;
