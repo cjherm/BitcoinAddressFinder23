c1e84f280dff229604beda1a108093bb82223349
cjherm <akbigchris@gmail.com>
2023-09-03 16:23:16 +0200

    Modified data structure "AddressBytes" to store private key and address only without results in between.

--- src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/AddressBytes.java
@@ -1,24 +1,47 @@
 package net.ladenthin.bitcoinaddressfinder;
 
-import java.math.BigInteger;
-
+/**
+ * Data structure to store a private key and the address derived from it.
+ */
 public class AddressBytes {
 
     public static final AddressBytes INVALID_ADDRESS = null;
     public static final int SINGLE_SHA256_BYTE_LEN = 32;
     public static final int DOUBLE_SHA256_BYTE_LEN = 2 * SINGLE_SHA256_BYTE_LEN;
     public static final int PUB_KEY_WITHOUT_PARITY_WITH_DOUBLE_SHA256_BYTE_LEN = PublicKeyBytes.TWO_COORDINATES_NUM_BYTES + DOUBLE_SHA256_BYTE_LEN;
-    // TODO store public key with parity in opencl kernel result
-    public static final int PUB_KEY_WITH_PARITY_WITH_SHA256_BYTE_LEN = PUB_KEY_WITHOUT_PARITY_WITH_DOUBLE_SHA256_BYTE_LEN + 1;
-    private final BigInteger privateKey;
-    private final byte[] uncompressed;
+    public static final int PRIVATE_KEY_NUM_BYTES = 32;
+    public static final int ADDRESS_NUM_BYTES = 25;
+    public static final int TOTAL_NUM_BYTES = PRIVATE_KEY_NUM_BYTES + ADDRESS_NUM_BYTES;
 
-    public AddressBytes(BigInteger privateKey, byte[] uncompressed) {
+    private final byte[] privateKey;
+    private final byte[] address;
+
+    /**
+     * Constructor for storing a private key and its address.
+     *
+     * @param privateKey The private key as a byte array
+     * @param address    The address as a byte array
+     */
+    public AddressBytes(byte[] privateKey, byte[] address) {
         this.privateKey = privateKey;
-        this.uncompressed = uncompressed;
+        this.address = address;
     }
 
-    public byte[] getUncompressed() {
-        return uncompressed;
+    /**
+     * The private key.
+     *
+     * @return private key as byte array
+     */
+    public byte[] getPrivateKey() {
+        return privateKey;
+    }
+
+    /**
+     * The address.
+     *
+     * @return address as byte array
+     */
+    public byte[] getAddress() {
+        return address;
     }
 }
