724fbf7350f8bd85aafca4316a8ff855905aa42b
cjherm <akbigchris@gmail.com>
2023-10-01 20:41:34 +0200

    Changed order of destination buffer size calculation in "OpenClTask".

--- src/main/java/net/ladenthin/bitcoinaddressfinder/OpenClTask.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/OpenClTask.java
@@ -88,10 +88,10 @@ public class OpenClTask {
     public int getDstSizeInBytes() {
         if (cProducer.kernelMode == OpenCLContext.GEN_XY_COORDINATES_ONLY_MODE) {
             return PublicKeyBytes.TWO_COORDINATES_NUM_BYTES * cProducer.getWorkSize();
-        } else if (cProducer.kernelMode == OpenCLContext.GEN_ADDRESSES_ONLY_MODE) {
-            return AddressBytesFactory.NUM_BYTES_TOTAL * cProducer.getWorkSize();
         } else if (cProducer.kernelMode == OpenCLContext.GEN_PUBLIC_KEY_ONLY_MODE) {
             return ResultBytesFactory.NUM_BYTES_TOTAL_UNTIL_PUBLIC_KEY * cProducer.getWorkSize();
+        } else if (cProducer.kernelMode == OpenCLContext.GEN_ADDRESSES_ONLY_MODE) {
+            return AddressBytesFactory.NUM_BYTES_TOTAL * cProducer.getWorkSize();
         } else if (cProducer.kernelMode == OpenCLContext.GEN_UNTIL_1ST_SHA256_MODE) {
             return ResultBytesFactory.NUM_BYTES_TOTAL_UNTIL_1ST_SHA256 * cProducer.getWorkSize();
         } else if (cProducer.kernelMode == OpenCLContext.GEN_UNTIL_RIPEMD160_MODE) {
