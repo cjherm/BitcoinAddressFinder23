419115d1b21ebf0428682ac3044d063daf319f27
cjherm <akbigchris@gmail.com>
2023-10-07 12:29:22 +0200

    Added implementation to "PublicKeysRound".

--- src/main/java/net/ladenthin/bitcoinaddressfinder/benchmark/types/rounds/PublicKeysRound.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/benchmark/types/rounds/PublicKeysRound.java
@@ -1,6 +1,7 @@
 package net.ladenthin.bitcoinaddressfinder.benchmark.types.rounds;
 
 import net.ladenthin.bitcoinaddressfinder.InvalidWorkSizeException;
+import net.ladenthin.bitcoinaddressfinder.PublicKeyBytes;
 import net.ladenthin.bitcoinaddressfinder.benchmark.BenchmarkLogger;
 import net.ladenthin.bitcoinaddressfinder.benchmark.MeasurementRound;
 import net.ladenthin.bitcoinaddressfinder.configuration.CProducerOpenCL;
@@ -9,12 +10,20 @@ import java.math.BigInteger;
 
 public class PublicKeysRound extends MeasurementRound {
 
+    byte[][] ripemd160BytesResults;
+
     public PublicKeysRound(CProducerOpenCL producerOpenCL, int roundsPerInitializedContext, String parameterToPrint, String parameterToLatex, BenchmarkLogger logger) {
         super(producerOpenCL, roundsPerInitializedContext, parameterToPrint, parameterToLatex, logger);
+        ripemd160BytesResults = new byte[producerOpenCL.getWorkSize()][PublicKeyBytes.HASH160_SIZE];
     }
 
     @Override
     protected byte[][] executeAndReturnResults(BigInteger[][] privateKeyChunks, int roundIndex) throws InvalidWorkSizeException {
-        return new byte[0][];
+        openCLGridResults[roundIndex] = openCLContext.createResult(privateKeyChunks[roundIndex]);
+        PublicKeyBytes[] publicKeyBytesResults = openCLGridResults[roundIndex].getPublicKeyBytes();
+        for (int i = 0; i < publicKeyBytesResults.length; i++) {
+            ripemd160BytesResults[i] = publicKeyBytesResults[i].getUncompressedKeyHash();
+        }
+        return ripemd160BytesResults;
     }
 }
