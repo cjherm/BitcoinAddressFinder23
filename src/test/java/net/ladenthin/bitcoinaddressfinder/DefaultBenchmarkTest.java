1b2738208246b70830d1f4c5eaf6d3d9939ea0bc
cjherm <akbigchris@gmail.com>
2023-10-07 13:46:13 +0200

    Added 5 additional tests to "DefaultBenchmarkTest".

--- src/test/java/net/ladenthin/bitcoinaddressfinder/DefaultBenchmarkTest.java
+++ src/test/java/net/ladenthin/bitcoinaddressfinder/DefaultBenchmarkTest.java
@@ -34,4 +34,114 @@ public class DefaultBenchmarkTest {
         // assert
         assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
     }
+
+    @Test
+    public void test_start_chunkMode_kernelMode0_10xCtx_10xtotalRounds() throws BenchmarkFactoryException {
+        // arrange
+        CBenchmark configFile = new CBenchmark();
+        configFile.type = BenchmarkFactory.TYPE_DEFAULT;
+        configFile.gridNumBits = BenchmarkFactory.DEFAULT_GRIDNUMBITS;
+        configFile.chunkMode = BenchmarkFactory.DEFAULT_CHUNKMODE;
+        configFile.kernelMode = OpenCLContext.GEN_XY_COORDINATES_ONLY_MODE;
+        configFile.totalRounds = BenchmarkFactory.DEFAULT_TOTAL_ROUNDS;
+        configFile.contextRounds = BenchmarkFactory.DEFAULT_CONTEXT_ROUNDS;
+        configFile.logToConsole = BenchmarkFactory.DEFAULT_LOGTOCONSOLE;
+        configFile.logToFile = false;
+        BenchmarkFactory factory = new BenchmarkFactory(configFile);
+        BenchmarkType runner = factory.createBenchmarkRunner();
+
+        // act
+        runner.start();
+
+        // assert
+        assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
+    }
+
+    @Test
+    public void test_start_chunkMode_kernelMode3_10xCtx_10xtotalRounds() throws BenchmarkFactoryException {
+        // arrange
+        CBenchmark configFile = new CBenchmark();
+        configFile.type = BenchmarkFactory.TYPE_DEFAULT;
+        configFile.gridNumBits = BenchmarkFactory.DEFAULT_GRIDNUMBITS;
+        configFile.chunkMode = BenchmarkFactory.DEFAULT_CHUNKMODE;
+        configFile.kernelMode = OpenCLContext.GEN_ADDRESSES_ONLY_MODE;
+        configFile.totalRounds = BenchmarkFactory.DEFAULT_TOTAL_ROUNDS;
+        configFile.contextRounds = BenchmarkFactory.DEFAULT_CONTEXT_ROUNDS;
+        configFile.logToConsole = BenchmarkFactory.DEFAULT_LOGTOCONSOLE;
+        configFile.logToFile = false;
+        BenchmarkFactory factory = new BenchmarkFactory(configFile);
+        BenchmarkType runner = factory.createBenchmarkRunner();
+
+        // act
+        runner.start();
+
+        // assert
+        assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
+    }
+
+    @Test
+    public void test_start_nonChunkMode_kernelMode2_10xCtx_10xtotalRounds() throws BenchmarkFactoryException {
+        // arrange
+        CBenchmark configFile = new CBenchmark();
+        configFile.type = BenchmarkFactory.TYPE_DEFAULT;
+        configFile.gridNumBits = BenchmarkFactory.DEFAULT_GRIDNUMBITS;
+        configFile.chunkMode = false;
+        configFile.kernelMode = BenchmarkFactory.DEFAULT_KERNELMODE;
+        configFile.totalRounds = BenchmarkFactory.DEFAULT_TOTAL_ROUNDS;
+        configFile.contextRounds = BenchmarkFactory.DEFAULT_CONTEXT_ROUNDS;
+        configFile.logToConsole = BenchmarkFactory.DEFAULT_LOGTOCONSOLE;
+        configFile.logToFile = false;
+        BenchmarkFactory factory = new BenchmarkFactory(configFile);
+        BenchmarkType runner = factory.createBenchmarkRunner();
+
+        // act
+        runner.start();
+
+        // assert
+        assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
+    }
+
+    @Test
+    public void test_start_nonChunkMode_kernelMode0_10xCtx_10xtotalRounds() throws BenchmarkFactoryException {
+        // arrange
+        CBenchmark configFile = new CBenchmark();
+        configFile.type = BenchmarkFactory.TYPE_DEFAULT;
+        configFile.gridNumBits = BenchmarkFactory.DEFAULT_GRIDNUMBITS;
+        configFile.chunkMode = false;
+        configFile.kernelMode = OpenCLContext.GEN_XY_COORDINATES_ONLY_MODE;
+        configFile.totalRounds = BenchmarkFactory.DEFAULT_TOTAL_ROUNDS;
+        configFile.contextRounds = BenchmarkFactory.DEFAULT_CONTEXT_ROUNDS;
+        configFile.logToConsole = BenchmarkFactory.DEFAULT_LOGTOCONSOLE;
+        configFile.logToFile = false;
+        BenchmarkFactory factory = new BenchmarkFactory(configFile);
+        BenchmarkType runner = factory.createBenchmarkRunner();
+
+        // act
+        runner.start();
+
+        // assert
+        assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
+    }
+
+    @Test
+    public void test_start_nonCunkMode_kernelMode3_10xCtx_10xtotalRounds() throws BenchmarkFactoryException {
+        // arrange
+        CBenchmark configFile = new CBenchmark();
+        configFile.type = BenchmarkFactory.TYPE_DEFAULT;
+        configFile.gridNumBits = BenchmarkFactory.DEFAULT_GRIDNUMBITS;
+        configFile.chunkMode = false;
+        configFile.kernelMode = OpenCLContext.GEN_ADDRESSES_ONLY_MODE;
+        configFile.totalRounds = BenchmarkFactory.DEFAULT_TOTAL_ROUNDS;
+        configFile.contextRounds = BenchmarkFactory.DEFAULT_CONTEXT_ROUNDS;
+        configFile.logToConsole = BenchmarkFactory.DEFAULT_LOGTOCONSOLE;
+        configFile.logToFile = false;
+        BenchmarkFactory factory = new BenchmarkFactory(configFile);
+        BenchmarkType runner = factory.createBenchmarkRunner();
+
+        // act
+        runner.start();
+
+        // assert
+        assertThat(runner.getTotalNumberOfResults(), equalTo(EXPECTED_RESULTS));
+    }
 }
